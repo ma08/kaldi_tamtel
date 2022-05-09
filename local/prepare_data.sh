6c88cef163322bc92b143a0a80c8a3d6cb971d4d
Sourya Kakarla <sourya4@ladduu.com>
2022-05-08 20:36:02 -0400

    Add changes for prepare_data and prepare_dict

--- local/prepare_data.sh
+++ local/prepare_data.sh
@@ -23,35 +23,35 @@ for set in dev test train; do
   # - <sil> -> null : remove marked <sil>, it is modelled implicitly (in kaldi)
   # - (...) -> null : remove utterance names from end-lines of train
   # - it 's -> it's : merge words that contain apostrophe (if compound in dictionary, local/join_suffix.py)
-  { # Add STM header, so sclite can prepare the '.lur' file
-    echo ';;
-;; LABEL "o" "Overall" "Overall results"
-;; LABEL "f0" "f0" "Wideband channel"
-;; LABEL "f2" "f2" "Telephone channel"
-;; LABEL "male" "Male" "Male Talkers"
-;; LABEL "female" "Female" "Female Talkers"
-;;'
-    # Process the STMs
-    cat db/TEDLIUM_release-3/legacy/$set/stm/*.stm | sort -k1,1 -k2,2 -k4,4n | \
-      sed -e 's:([^ ]*)$::' | \
-      awk '{ $2 = "A"; print $0; }'
-  } | local/join_suffix.py > data/$set.orig/stm
+#   { # Add STM header, so sclite can prepare the '.lur' file
+#     echo ';;
+# ;; LABEL "o" "Overall" "Overall results"
+# ;; LABEL "f0" "f0" "Wideband channel"
+# ;; LABEL "f2" "f2" "Telephone channel"
+# ;; LABEL "male" "Male" "Male Talkers"
+# ;; LABEL "female" "Female" "Female Talkers"
+# ;;'
+#     # Process the STMs
+#     cat db/TEDLIUM_release-3/legacy/$set/stm/*.stm | sort -k1,1 -k2,2 -k4,4n | \
+#       sed -e 's:([^ ]*)$::' | \
+#       awk '{ $2 = "A"; print $0; }'
+#   } | local/join_suffix.py > data/$set.orig/stm
 
   # Prepare 'text' file
   # - {NOISE} -> [NOISE] : map the tags to match symbols in dictionary
-  cat $dir/stm | grep -v -e 'ignore_time_segment_in_scoring' -e ';;' | \
-    awk '{ printf ("%s-%07d-%07d", $1, $4*100, $5*100);
-           for (i=7;i<=NF;i++) { printf(" %s", $i); }
-           printf("\n");
-         }' | tr '{}' '[]' | sort -k1,1 > $dir/text || exit 1
+  # cat $dir/stm | grep -v -e 'ignore_time_segment_in_scoring' -e ';;' | \
+  #   awk '{ printf ("%s-%07d-%07d", $1, $4*100, $5*100);
+  #          for (i=7;i<=NF;i++) { printf(" %s", $i); }
+  #          printf("\n");
+  #        }' | tr '{}' '[]' | sort -k1,1 > $dir/text || exit 1
 
-  # Prepare 'segments', 'utt2spk', 'spk2utt'
-  cat $dir/text | cut -d" " -f 1 | awk -F"-" '{printf("%s %s %07.2f %07.2f\n", $0, $1, $2/100.0, $3/100.0)}' > $dir/segments
-  cat $dir/segments | awk '{print $1, $2}' > $dir/utt2spk
-  cat $dir/utt2spk | utils/utt2spk_to_spk2utt.pl > $dir/spk2utt
+  # # Prepare 'segments', 'utt2spk', 'spk2utt'
+  # cat $dir/text | cut -d" " -f 1 | awk -F"-" '{printf("%s %s %07.2f %07.2f\n", $0, $1, $2/100.0, $3/100.0)}' > $dir/segments
+  # cat $dir/segments | awk '{print $1, $2}' > $dir/utt2spk
+  # cat $dir/utt2spk | utils/utt2spk_to_spk2utt.pl > $dir/spk2utt
 
-  # Prepare 'wav.scp', 'reco2file_and_channel'
-  cat $dir/spk2utt | awk -v set=$set -v pwd=$PWD '{ printf("%s sph2pipe -f wav -p %s/db/TEDLIUM_release-3/legacy/%s/sph/%s.sph |\n", $1, pwd, set, $1); }' > $dir/wav.scp
+  # # Prepare 'wav.scp', 'reco2file_and_channel'
+  # cat $dir/spk2utt | awk -v set=$set -v pwd=$PWD '{ printf("%s sph2pipe -f wav -p %s/db/TEDLIUM_release-3/legacy/%s/sph/%s.sph |\n", $1, pwd, set, $1); }' > $dir/wav.scp
   cat $dir/wav.scp | awk '{ print $1, $1, "A"; }' > $dir/reco2file_and_channel
 
   # Create empty 'glm' file
@@ -61,12 +61,12 @@ for set in dev test train; do
 
   # The training set seems to not have enough silence padding in the segmentations,
   # especially at the beginning of segments.  Extend the times.
-  if [ $set == "train" ]; then
-    mv data/$set.orig/segments data/$set.orig/segments.temp
-    utils/data/extend_segment_times.py --start-padding=0.15 \
-      --end-padding=0.1 <data/$set.orig/segments.temp >data/$set.orig/segments || exit 1
-    rm data/$set.orig/segments.temp
-  fi
+  # if [ $set == "train" ]; then
+  #   mv data/$set.orig/segments data/$set.orig/segments.temp
+  #   utils/data/extend_segment_times.py --start-padding=0.15 \
+  #     --end-padding=0.1 <data/$set.orig/segments.temp >data/$set.orig/segments || exit 1
+  #   rm data/$set.orig/segments.temp
+  # fi
 
   # Check that data dirs are okay!
   utils/validate_data_dir.sh --no-feats $dir || exit 1
