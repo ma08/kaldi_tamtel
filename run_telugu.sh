98a87b1b7ab84d8eee0e3150dce0be3dfdd649d0
Sourya Kakarla <sourya4@ladduu.com>
2022-06-13 16:32:02 -0400

    Add run changes for telugu stage 10

--- run_telugu.sh
+++ run_telugu.sh
@@ -53,7 +53,7 @@ train_lm=true
 
 home_folder=$HOME
 
-stage=9
+stage=10
 if [ $stage -le 0 ]; then
   input_dataset=telugu_combined_transcription
   #input_dataset=mozillacv_tamil/transcription
@@ -194,7 +194,6 @@ if [ $stage -le 9 ]; then
   date
   echo "----------------------- Stage $stage end---------------------------";
 fi
-exit 1
 
 if [ $stage -le 10 ]; then
   echo "----------------------- Stage $stage begin---------------------------";
@@ -212,6 +211,7 @@ if [ $stage -le 10 ]; then
   date
   echo "----------------------- Stage $stage end---------------------------";
 fi
+exit 1
 
 if [ $stage -le 11 ]; then
   echo "----------------------- Stage $stage begin---------------------------";
