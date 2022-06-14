a2955babe86fbba82c242e843e335d33e25224c2
Sourya Kakarla <sourya4@ladduu.com>
2022-06-13 20:43:31 -0400

    Add run changes for stage 14 telugu

--- run_telugu.sh
+++ run_telugu.sh
@@ -53,7 +53,7 @@ train_lm=true
 
 home_folder=$HOME
 
-stage=13
+stage=14
 if [ $stage -le 0 ]; then
   input_dataset=telugu_combined_transcription
   #input_dataset=mozillacv_tamil/transcription
@@ -253,7 +253,6 @@ if [ $stage -le 13 ]; then
   date
   echo "----------------------- Stage $stage end---------------------------";
 fi
-exit 1
 
 if [ $stage -le 14 ]; then
   echo "----------------------- Stage $stage begin---------------------------";
@@ -276,6 +275,7 @@ if [ $stage -le 14 ]; then
   date
   echo "----------------------- Stage $stage end---------------------------";
 fi
+exit 1
 
 if [ $stage -le 15 ]; then
   echo "----------------------- Stage $stage begin---------------------------";
